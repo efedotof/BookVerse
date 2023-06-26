1f97db5769d8b000332b12dc0623e601ee68010b
Egor <fedmehol@icloud.com>
2023-06-26 12:47:23 +0300

    change page2

--- lib/widgets/list_page_2.dart
+++ lib/widgets/list_page_2.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
-
+import 'dart:io' show Platform;
 import 'cardsing.dart';
 
+int colimns = 2;
+
+void rows_colst(){
+  if (Platform.isAndroid) {
+    colimns = 2;
+  }
+  else if(Platform.isLinux) {
+    colimns = 4;
+  }
+  else if(Platform.isWindows) {
+    colimns = 4;
+  }
+  else if(Platform.isIOS) {
+    colimns = 2;
+  }
+  else if(Platform.isMacOS) {
+    colimns = 4;
+  }
+
+}
+
 Widget List_views_2(BuildContext context){
+  rows_colst();
   return 
       Center(
         child: GridView.count(
           shrinkWrap: true,
-          crossAxisCount: 2,
-          padding: EdgeInsets.all(25),
+          crossAxisCount: colimns,
+          padding: EdgeInsets.all(13),
           children: List.generate(20, (index){
-            return Container(
-              height: 300,
-              width: 150,
-              child: cards(context),
-            ); 
+            return Card(
+                    child: Container(
+                      height: 290,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20)),
+                  
+                      child: Stack(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                child: Image.asset('assets/image_templets_books.jpg', fit: BoxFit.fill,)
+                              ),
+                              
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
           }),
           
         ),
