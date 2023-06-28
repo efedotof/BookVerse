e95286345fbf657186ed867310cfbb1e8e0fc61d
Egor <fedmehol@icloud.com>
2023-06-28 09:39:37 +0300

    change cards_reade_todo

--- lib/widgets/cards_reade_todo.dart
+++ lib/widgets/cards_reade_todo.dart
@@ -1,5 +1,39 @@
 import 'package:flutter/material.dart';
 
 Widget cards_todo(BuildContext context){
-  return null;
+  return Container(
+    width: 150,
+    height: 400,
+    
+     decoration: BoxDecoration(
+  ),
+    child: Card(
+       elevation: 0,
+      child: Column(
+        children: [
+          Container(
+             padding: EdgeInsets.all(16.0),
+             alignment: Alignment.centerLeft,
+             child: Center(child: Text("Текущее")),
+           ), 
+          Container(
+            height: 200 - 45,
+            width: 150,
+            child: IconButton(
+                  onPressed: (){
+                    null;
+                  },
+                  icon: Image.asset('assets/image_templets_books.jpg', fit: BoxFit.fill,),
+                ),
+          ),
+          Container(
+             padding: EdgeInsets.all(16.0),
+             alignment: Alignment.centerLeft,
+             child: Center(child: Text("name")),
+           ), 
+        ],
+      ),
+      
+    ),
+  );
 }
