77da816229cc525f5e0cc6aac87e7f0bcedc024c
Egor <fedmehol@icloud.com>
2023-07-29 13:02:23 +0300

    fixed scrolling and showing the action menu

--- lib/widgets/list_page/list_page_1.dart
+++ lib/widgets/list_page/list_page_1.dart
@@ -1,7 +1,18 @@
+/********************************************/
+/*                                          */
+/*        📱 Название приложения: BookVerse      */
+/*        📚 Описание: Это интуитивное мобильное приложение для чтения и публикации книг. Откройте мир литературы прямо с вашего устройства, где бы вы ни находились. С легкостью загружайте и читайте свои любимые книги из памяти устройства в различных форматах.    */
+/*        👤 Автор: efedotov                       */
+/*        📅 Дата: 2023-07-29                 */
+/*        🚀 Версия: 0.0                         */
+/*                                          */
+/*        © Все права защищены.                */
+/*                                          */
+/// *****************************************
+
+
 import 'package:flutter/material.dart';
 import 'package:magazine/widgets/until/search.dart';
-import 'package:searchbar_animation/searchbar_animation.dart';
-import 'package:anim_search_bar/anim_search_bar.dart';
 
 import '../additional tools/cardsing.dart';
 import '../additional tools/list_data_text.dart';
@@ -9,8 +20,10 @@ import '../until/AdditionalTitleText.dart';
 import '../until/Textheader.dart';
 import '../until/description.dart';
 
+// ignore: camel_case_types
 class List_views extends StatefulWidget {
   const List_views({super.key});
+  // ignore: library_private_types_in_public_api
   static _List_viewsState? of(BuildContext context) {
     return context.findAncestorStateOfType<_List_viewsState>();
   }
@@ -19,90 +32,93 @@ class List_views extends StatefulWidget {
   State<List_views> createState() => _List_viewsState();
 }
 
+// ignore: camel_case_types
 class _List_viewsState extends State<List_views> {
   bool isShowDescription = false;
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      body: Stack(
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: SingleChildScrollView(
+      child: Stack(
         children: [
-          ListView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Textheader(text: "Библиотека"),
-              ),
-              search(),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: AdditionalTitleText(text: "Новинки"),
-              ),
-              Container(
-                height: 300,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    for (int i = 0; i < book_as_famali.length; i++)
-                      cards(
-                        books: book_as_famali,
-                        indexs: i,
-                        image_netvorsk:
-                            "https://marketplace.canva.com/EADx4IPoPNo/1/0/1024w/canva-%D0%BE%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8-%D0%B2-%D0%B6%D0%B0%D0%BD%D1%80%D0%B5-%D1%82%D1%80%D0%B8%D0%BB%D0%BB%D0%B5%D1%80%D0%B0-%D1%81-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5%D0%BC-%D0%BC%D0%BE%D0%BD%D0%BE%D1%85%D1%80%D0%BE%D0%BC%D0%BD%D0%BE%D0%B9-%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8-%D0%BB%D0%B5%D1%81%D0%B0-iRBldJ_jyLw.jpg",
-                      ),
-                  ],
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: ListView(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Textheader(text: "Библиотека"),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: AdditionalTitleText(text: "Все книги"),
-              ),
-              //gridViews
-              Column(
-                children: [
-                  Expanded(
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2, // Number of columns in the grid
-                        crossAxisSpacing:
-                            16.0, // Increase this value to add more space between columns
-                        mainAxisSpacing:
-                            16.0, // Increase this value to add more space between rows
-                      ),
-                      itemCount:
-                          book_as_famali.length, // Total number of items in the grid
-                      shrinkWrap: true, // Disable scrolling of the GridView
-                      itemBuilder: (context, index) {
-                        // You can create a custom widget here for each grid item, similar to "cards"
-                        return Center(
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                                top: 0, bottom: 0), // Add padding around each item
-                            child: Container(
-                              height: 425,
-                              child: cards(
-                                books: book_as_famali,
-                                indexs: index,
-                                image_netvorsk:
-                                    "https://marketplace.canva.com/EADx4IPoPNo/1/0/1024w/canva-%D0%BE%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8-%D0%B2-%D0%B6%D0%B0%D0%BD%D1%80%D0%B5-%D1%82%D1%80%D0%B8%D0%BB%D0%BB%D0%B5%D1%80%D0%B0-%D1%81-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5%D0%BC-%D0%BC%D0%BE%D0%BD%D0%BE%D1%85%D1%80%D0%BE%D0%BC%D0%BD%D0%BE%D0%B9-%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8-%D0%BB%D0%B5%D1%81%D0%B0-iRBldJ_jyLw.jpg",
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
+                const search(),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: AdditionalTitleText(text: "Новинки"),
+                ),
+                Container(
+                  height: 300,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      for (int i = 0; i < book_as_famali.length; i++)
+                        cards(
+                          books: book_as_famali,
+                          indexs: i,
+                          image_netvorsk:
+                              "https://marketplace.canva.com/EADx4IPoPNo/1/0/1024w/canva-%D0%BE%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8-%D0%B2-%D0%B6%D0%B0%D0%BD%D1%80%D0%B5-%D1%82%D1%80%D0%B8%D0%BB%D0%BB%D0%B5%D1%80%D0%B0-%D1%81-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5%D0%BC-%D0%BC%D0%BE%D0%BD%D0%BE%D1%85%D1%80%D0%BE%D0%BC%D0%BD%D0%BE%D0%B9-%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8-%D0%BB%D0%B5%D1%81%D0%B0-iRBldJ_jyLw.jpg",
+                        ),
+                    ],
                   ),
-                ],
-              ),
-            ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: AdditionalTitleText(text: "Все книги"),
+                ),
+                // GridView
+                GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Number of columns in the grid
+                    crossAxisSpacing:
+                        16.0, // Increase this value to add more space between columns
+                    mainAxisSpacing:
+                        16.0, // Increase this value to add more space between rows
+                  ),
+                  itemCount: book_as_famali.length, // Total number of items in the grid
+                  shrinkWrap: true, // Disable scrolling of the GridView
+                  physics: NeverScrollableScrollPhysics(), // Disable GridView scrolling
+                  itemBuilder: (context, index) {
+                    // You can create a custom widget here for each grid item, similar to "cards"
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 0, bottom: 0), // Add padding around each item
+                        child: SizedBox(
+                          height: 425,
+                          child: cards(
+                            books: book_as_famali,
+                            indexs: index,
+                            image_netvorsk:
+                                "https://marketplace.canva.com/EADx4IPoPNo/1/0/1024w/canva-%D0%BE%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8-%D0%B2-%D0%B6%D0%B0%D0%BD%D1%80%D0%B5-%D1%82%D1%80%D0%B8%D0%BB%D0%BB%D0%B5%D1%80%D0%B0-%D1%81-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5%D0%BC-%D0%BC%D0%BE%D0%BD%D0%BE%D1%85%D1%80%D0%BE%D0%BC%D0%BD%D0%BE%D0%B9-%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8-%D0%BB%D0%B5%D1%81%D0%B0-iRBldJ_jyLw.jpg",
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
           if (isShowDescription) const DescriptionBooks(),
         ],
       ),
-    );
-  }
+    ),
+  );
+}
 
+
+
+  // ignore: non_constant_identifier_names
   void init_isShowDescription() {
     setState(() {
       isShowDescription = !isShowDescription;
--- lib/widgets/until/AddFile.dart
+++ lib/widgets/until/AddFile.dart
@@ -1,5 +1,17 @@
 
+// ignore_for_file: file_names
 
+/********************************************/
+/*                                          */
+/*        📱 Название приложения: BookVerse      */
+/*        📚 Описание: Это интуитивное мобильное приложение для чтения и публикации книг. Откройте мир литературы прямо с вашего устройства, где бы вы ни находились. С легкостью загружайте и читайте свои любимые книги из памяти устройства в различных форматах.    */
+/*        👤 Автор: efedotov                       */
+/*        📅 Дата: 2023-07-29                 */
+/*        🚀 Версия: 0.0                         */
+/*                                          */
+/*        © Все права защищены.                */
+/*                                          */
+/// *****************************************
 
 import 'package:flutter/material.dart';
 import 'package:magazine/widgets/list_page/list_page_2.dart';
@@ -27,62 +39,21 @@ class _AddFileState extends State<AddFile> {
 
           },)),
           Container(
-                  height: MediaQuery.of(context).size.height / 2,
+                  height: MediaQuery.of(context).size.height / 5,
                   width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(20.0),
                       topRight: Radius.circular(20.0),
                     ),
                   ),
-                  child: Row(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      // Image on the left
-                      Container(
-                        width: MediaQuery.of(context).size.width / 3,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(
-                              'https://marketplace.canva.com/EADx4IPoPNo/1/0/1024w/canva-%D0%BE%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8-%D0%B2-%D0%B6%D0%B0%D0%BD%D1%80%D0%B5-%D1%82%D1%80%D0%B8%D0%BB%D0%BB%D0%B5%D1%80%D0%B0-%D1%81-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5%D0%BC-%D0%BC%D0%BE%D0%BD%D0%BE%D1%85%D1%80%D0%BE%D0%BC%D0%BD%D0%BE%D0%B9-%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8-%D0%BB%D0%B5%D1%81%D0%B0-iRBldJ_jyLw.jpg',
-                            ),
-                            fit: BoxFit.cover,
-                          ),
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20.0),
-                           
-                          ),
-                        ),
-                      ),
-                      // Details on the right
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Автор', style: TextStyle(fontSize: 18)),
-                              SizedBox(height: 8),
-                              Text('Жанр', style: TextStyle(fontSize: 18)),
-                              SizedBox(height: 16),
-                              Expanded(
-                                child: SingleChildScrollView(
-                                  child: Text(
-                                    'Тут будет краткое описание книги.',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Handle the button tap here
-                                },
-                                child: Text('Читать сейчас'),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                      OutlinedButton(onPressed: (){}, child: const Text('Загрузить из файла')),
+                      OutlinedButton(onPressed: (){}, child: const Text('Загрузить свою историю')),
+                    
                     ],
                   ),
                 ),
